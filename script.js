924b5a1383830919f5533d1c1f720826837f0135
yoan813 <yoan.leulier@orange.fr>
2022-11-17 20:17:19 +0100

    Actualy function lancedés and add neutral score

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Récupération du DOM
 const newgame = document.getElementById('new-game');
 const dice = document.getElementById('facedice');
 const player1 = document.getElementById('play1');
@@ -7,30 +8,29 @@ const round2 = document.getElementById('score2');
 const roll = document.getElementById('roll');
 const hold = document.getElementById('hold');
 
+//Initialisation de la fonction
 function lancedés(min,max)
 {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+    let lancer = Math.floor(Math.random() * (max - min + 1)) + min;
+    let contentdice = dice.innerHTML
 
-let lancer = lancedés(1, 6);
-
-let contentdice = dice.innerHTML
-
-if (lancer == 1) {
-    dice.innerHTML = "&#9856";
-} else if (lancer == 2) {
+    if (lancer == 1) {
+    dice.innerHTML = "&#9856";  
+    } else if (lancer == 2) {
     dice.innerHTML = "&#9857";
-} else if (lancer == 3) {
+    } else if (lancer == 3) {
     dice.innerHTML = "&#9858";
-} else if (lancer == 4) {
+    } else if (lancer == 4) {
     dice.innerHTML = "&#9859";
-} else if (lancer == 5) {
+    } else if (lancer == 5) {
     dice.innerHTML = "&#9860";
-} else if (lancer == 6) {
+    } else if (lancer == 6) {
     dice.innerHTML = "&#9861";
-} else {
+    } else {
    alert("Une erreur c'est produite !")
 };
+};
 
-console.log(lancer);
+// délimitation des nombres et appel onclick en HTML 
+lancedés(1, 6);
 
